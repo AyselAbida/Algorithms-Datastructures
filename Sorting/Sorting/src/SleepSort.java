4589b78e26541189888c02e44e704187373a3a91
Abida <abidamohammadi97@gmail.com>
2021-01-13 16:50:29 +1100

    sorting

---
+++ Sorting/Sorting/src/SleepSort.java
@@ -0,0 +1,35 @@
+
+
+import java.util.concurrent.CountDownLatch;
+
+public class SleepSort {
+	
+	public static void main(String[] args) {
+		int[] nums = { 1, 4, 2, 6, 8, 5, 6, 9 };
+		sleepSortAndPrint(nums);
+	}
+
+	public static void sleepSortAndPrint(int[] nums) {
+
+        final CountDownLatch doneSignal = new CountDownLatch(nums.length);
+
+        for (final Integer num : nums) {
+            new Thread(new Runnable() {
+                public void run() {
+                    doneSignal.countDown();
+                    try {
+                        doneSignal.await();
+
+                        // using straight milliseconds produces unpredictable
+                        // results with small numbers
+                        // using 1000 here gives a nifty demonstration
+                        Thread.sleep(num * 100);
+                        System.out.print(num + "  ");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+}
